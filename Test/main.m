f63d1f5f8988198a827eaf0de3ac66fc493e3071
remantou <Tony@tonydemacbook-pro.local>
2016-10-24 20:55:06 +0800

    Initial Commit

---
+++ Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Tony on 2016/10/24.
+//  Copyright © 2016年 www.tony.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
